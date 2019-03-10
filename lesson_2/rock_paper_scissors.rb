474040f97c08ae6ccacaff31f487f23039018676
Diana <dianarinconbl@gmail.com>
2019-03-10 15:21:30 +1100

    Refactored RSP game

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,60 +1,71 @@
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICES ={rock: "r", paper: "p", scissors: "sc", spock: "sp", lizard: "l"}
 
 def prompt(message)
   puts "=> #{message}"
 end
 
-# SEE BELOW THE REFACTORING OF THIS LOGIC
-# def display_result(player, computer)
-#   if    (player == "rock" && computer == "scissors") ||
-#         (player == "paper" && computer == "rock") ||
-#         (player == "scissors" && computer == "paper")
-#     prompt "You won!"
-#   elsif (player == "rock" && computer == "paper") ||
-#         (player == "paper" && computer == "scissors") ||
-#         (player == "scissors" && computer == "rock")
-#     prompt "Computer won!"
-#   else
-#     prompt "It's a tie!"
-#   end
-# end
-
-def win?(first, second)
-  (first == "rock" && second == "scissors") ||
-    (first == "paper" && second == "rock") ||
-    (first == "scissors" && second == "paper")
-end
+prompt "Hello! Welcome to the Rock, Paper, Scissors, Spock, Lizard GAME"
 
 def display_result(player, computer)
-  if win?(player, computer)
+  hash = {r:  ["l", "sc"],
+          p:  ["r", "sp"],
+          sc: ["p", "l"],
+          l:  ["sp", "p"],
+          sp: ["r", "sc"]
+  }
+
+  if hash[player.to_sym].include?(computer)
     prompt "You won!"
-  elsif win?(computer, player)
+  elsif hash[computer.to_sym].include?(player)
     prompt "Computer won!"
   else
     prompt "It's a tie!"
   end
 end
 
+
 loop do
   choice = ''
   loop do
-    prompt "Choose one: #{VALID_CHOICES.join(', ')}"
+    choice_prompt = <<-MSG
+      Please choose one option as follows:
+      1) rock: r
+      2) paper: p
+      3) scissors: sc
+      4) spock: sp
+      5) lizard: l
+    MSG
+
+  prompt(choice_prompt)
+
     choice = gets.chomp
 
-    break if VALID_CHOICES.include?(choice)
+    break if VALID_CHOICES.values.include?(choice)
     prompt "Not a valid choice, try again!"
   end
 
-  computer_choice = VALID_CHOICES.sample
+  computer_choice = VALID_CHOICES.values.sample
 
   prompt "You chose: #{choice}; Computer chose: #{computer_choice}"
 
   display_result(choice, computer_choice)
 
-  prompt "Do you want to play again? (y or n)"
-  play_again = gets.chomp
+  play_again = ''
+  prompt "Do you want to play again? (Y or N)"
 
-  break unless play_again == "y"
+  loop do
+    play_again = gets.chomp
+
+    if play_again.downcase == "y"
+      break
+    elsif play_again.downcase == "n"
+      break
+    else
+      prompt "Please enter: Y to keep playing or N to exit the game."
+    end
+  end
+
+  break unless play_again.downcase == "y"
 end
 
 prompt "Thank you for playing. Good bye!!!"
