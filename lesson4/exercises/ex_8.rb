fff3f6b50b679295c0eed4108dc5b7903fdbaf09
Diana <dianarinconbl@gmail.com>
2019-03-26 19:52:12 +1100

    Update ex 8

--- lesson4/exercises/ex_8.rb
+++ lesson4/exercises/ex_8.rb
@@ -1,17 +1,16 @@
 numbers = [1, 2, 3, 4]
 numbers.each do |number|
   p number
-  # numbers.shift(1)
-  puts '====='
-  p numbers
+  numbers.shift(1)
 end
 
-#[1,2,3,4]
+#Firstly at index 0 the element is 1, but when that number is removed then at index 1 will be 3.
+#[1,3]
 
-# numbers = [1, 2, 3, 4]
-# numbers.each do |number|
-#   p number
-#   numbers.pop(1)
-# end
+numbers = [1, 2, 3, 4]
+numbers.each do |number|
+  p number
+  numbers.pop(1)
+end
 
 #[1,2]
