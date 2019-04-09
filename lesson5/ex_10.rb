6912362fa1d94d0c2d8a730967989410f1b0063a
Diana <dianarinconbl@gmail.com>
2019-04-09 18:35:15 +1000

    ex 11, 12

--- lesson5/ex_10.rb
+++ lesson5/ex_10.rb
@@ -4,10 +4,10 @@
 
 
 [{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}].map do |hash|
- new_hash = {}
+  new_hash = {}
   hash.each do |key, value|
     new_hash[key] = value + 1
   end
 
-  p new_hash
+new_hash
 end
