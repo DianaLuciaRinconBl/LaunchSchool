d17c3c05437568264ed04e6f8e00ccd6ff244067
Diana <dianarinconbl@gmail.com>
2019-04-18 18:53:38 +1000

    Update TTT...methods and loops included

--- lesson_6/tictactoe.rb
+++ lesson_6/tictactoe.rb
@@ -1,8 +1,15 @@
+require 'pry'
+
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
 def prompt(msg)
   puts "=> #{msg}"
 end
 
 def display_board(brd)
+  system 'clear'
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -20,21 +27,59 @@ end
 
 def initialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] = " "}
+  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
   new_board
 end
 
-def player_places_piece(brd)
-
-
-  loop do
-    prompt "Choose a square (1-9):"
-    square = gets.chomp.to_i
-  end
-
-    brd[square] = 'X'
+def empty_squares(brd)
+  brd.keys.select {|num| brd[num] == INITIAL_MARKER}
 end
 
+def player_places_piece!(brd)
+  square = ''
+  loop do
+    prompt "Choose a square #{empty_squares(brd).join(', ')}:"
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    prompt "Sorry, that's not a valid choice"
+  end
+
+    brd[square] = PLAYER_MARKER
+end
+
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  !!detect_winner(brd)
+end
+
+def detect_winner(brd)
+  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
+                  [[1, 5, 9], [3, 5, 7]]
+end
 
 board = initialize_board
+display_board(board)
+
+loop do
+  player_places_piece!(board)
+  computer_places_piece!(board)
+  display_board(board)
+  break if someone_won?(board) || board_full?(board)
+end
+
+if someone_won?(board)
+  prompt "#{detect_winner(board)} won!"
+else
+  prompt "It's a tie!"
+end
+
 display_board(board)
